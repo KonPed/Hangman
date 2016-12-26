d248a05f7e02697872b89b413da3b95fab99181e
Konstantinos <kpaidarakis@gmail.com>
2016-12-27 00:28:05 +0200

    add applyGuess method for guessing a letter against the answer | store the letter in hits or misses

--- src/Game.java
+++ src/Game.java
@@ -3,8 +3,22 @@
  */
 public class Game {
     private String answer;
+    private String hits;
+    private String misses;
 
     public Game(String answer) {
         this.answer = answer;
+        hits = "";
+        misses = "";
+    }
+
+    public boolean applyGuess(char letter) {
+        boolean validGuess = (answer.indexOf(letter) != -1);
+        if (validGuess) {
+            hits += letter;
+        } else {
+            misses += letter;
+        }
+        return validGuess;
     }
 }
--- src/Hangman.java
+++ src/Hangman.java
@@ -4,5 +4,6 @@
 public class Hangman {
     public static void main(String[] args) {
         Game game = new Game("midnight");
+        System.out.println(game.applyGuess('m'));
     }
 }
