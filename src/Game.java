293d758f85f70b1c53546ddc8554ab1cd3e321b2
Konstantinos <kpaidarakis@gmail.com>
2016-12-26 21:02:12 +0200

    Hangman class(main) created | constructor on Game class to initialize the answer

--- src/Game.java
+++ src/Game.java
@@ -2,5 +2,9 @@
  * Created by konstantinos on 12/26/2016.
  */
 public class Game {
+    private String answer;
 
+    public Game(String answer) {
+        this.answer = answer;
+    }
 }
---
+++ src/Hangman.java
@@ -0,0 +1,8 @@
+/**
+ * Created by konstantinos on 12/26/2016.
+ */
+public class Hangman {
+    public static void main(String[] args) {
+
+    }
+}
