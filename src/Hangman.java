def44d8cf40ae3d4a563170b48a2cbb324ccf039
Konstantinos <kpaidarakis@gmail.com>
2016-12-27 22:58:11 +0200

    made the program to check for arguments in the main method, then start the game, or display instructions.

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 /**
  * Created by konstantinos on 12/26/2016.
  */
 public class Hangman {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: type java Hangman and then the <answer> || set the answer through IDE's program arguments");
+            System.out.println("HAVE FUN!!!");
+            System.err.println("answer is required");
+            System.err.println("Set the answer in the program arguments");
+            System.exit(1);
+        }
         Game game = new Game(args[0]);
         Prompter prompter = new Prompter(game);
         while (game.remainingTimes() != 0 && !game.isWon()) {
