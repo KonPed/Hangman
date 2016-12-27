0ee6d487a170936513f9e277bf2631cf212d8b03
Konstantinos <kpaidarakis@gmail.com>
2016-12-27 13:06:33 +0200

    rewrite the promptForGuess method using Scanner instead of BufferedReader.

--- src/Prompter.java
+++ src/Prompter.java
@@ -1,28 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * Created by konstantinos on 12/26/2016.
  */
 public class Prompter {
     private Game game;
-    BufferedReader br;
 
     public Prompter(Game game) {
         this.game = game;
-        br = new BufferedReader(new InputStreamReader(System.in));
     }
 
     public boolean promptForGuess() {
-        char guess = 0;
-        System.out.println("Enter a letter ");
-        try {
-            String guessInput = br.readLine();
-            guess = guessInput.charAt(0);
-        } catch (IOException | IndexOutOfBoundsException e) {
-            System.out.println("Wow " + e.getMessage());
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a letter: ");
+        String guessInput = scanner.next();
+        char guess = guessInput.charAt(0);
         return game.applyGuess(guess);
     }
 }
